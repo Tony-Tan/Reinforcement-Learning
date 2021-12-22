635916280da38f21c8cb4bcde1d4ba7c53e643a2
Anthony Tan <tony.face2ai@gmail.com>
2021-12-22 21:06:32 +0800

    REINFORCE algorithm

--- simple_statistical_gradient_following_algorithm_for_connectionist/associate_task.py
+++ simple_statistical_gradient_following_algorithm_for_connectionist/associate_task.py
@@ -2,12 +2,11 @@ from k_arm_bandit import KArmedBandit
 import numpy as np
 import matplotlib.pyplot as plt
 
-
 SQRT_PI = np.sqrt(np.pi)
 
 
 def squashing_function(input):
-    return 1./(1.+np.exp(-input))
+    return 1. / (1. + np.exp(-input))
 
 
 class GaussianUnit:
@@ -20,24 +19,26 @@ class GaussianUnit:
         pass
 
     def __call__(self, input):
-        self.mean = squashing_function(self.weights_mean.dot(input))*10
+        self.mean = squashing_function(self.weights_mean.dot(input)) * 10
         # self.std = squashing_function(self.weights_std.dot(input))*10 + 0.01
-        action_prob = 1/(2*SQRT_PI*self.std)*np.exp(-0.5*((self.action_space-self.mean)/self.std)**2)+0.01
+        action_prob = 1 / (2 * SQRT_PI * self.std) * np.exp(
+            -0.5 * ((self.action_space - self.mean) / self.std) ** 2) + 0.01
         action_prob /= np.sum(action_prob)
-
+        # print(action_prob)
         return np.random.choice(self.action_space, 1, p=action_prob)[0]
 
     def characteristic_eligibility(self, x, y):
-        partial_mu = (y-self.mean)/(self.std**2)
+        partial_mu = (y - self.mean) / (self.std ** 2)
         # partial_std = ((y-self.mean)**2-self.std**2)/(self.std**3)
-        nabla_weights_mean = partial_mu * x
+        s = self.weights_mean.dot(x)
+        nabla_weights_mean = partial_mu * x * (np.exp(-s)/(1+np.exp(-s))**2)
         # nabla_weights_std = partial_std * x
-        return nabla_weights_mean #, nabla_weights_std
+        return nabla_weights_mean  # , nabla_weights_std
 
 
-def reinforce_algorithm(repeat_times=10000):
+def reinforce_algorithm(repeat_times=100000):
     # 10 arm bandits
-    K = 10
+    K = 5
     reward_log = []
     env_mean = np.random.normal(.0, 1.0, K)
 
@@ -57,11 +58,14 @@ def reinforce_algorithm(repeat_times=10000):
         # nabla_mean, nabla_std = gu.characteristic_eligibility(x, y)
         nabla_mean = gu.characteristic_eligibility(x, y)
         # gu.weights_std += (alpha_std * (r-base_line_std)*nabla_std)
-        gu.weights_mean += (alpha_mean * (r-base_line_mean)*nabla_mean)
+        gu.weights_mean += (alpha_mean * (r - base_line_mean) * nabla_mean)
         if repeat_i % 100 == 0:
             reward_log.append(r)
+    print('mean:')
     print(gu.mean)
-    print(gu.weights_mean)
+    # print('weights for mean:')
+    # print(gu.weights_mean)
+    print('environment mean:')
     print(env_mean)
     plt.plot(reward_log)
     plt.show()
@@ -69,8 +73,3 @@ def reinforce_algorithm(repeat_times=10000):
 
 if __name__ == '__main__':
     reinforce_algorithm()
-
-
-
-
-
