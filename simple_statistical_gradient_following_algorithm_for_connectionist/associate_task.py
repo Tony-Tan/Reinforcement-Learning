a23378ce8cd459f3c36a6e4265a808b11c5739e5
anthony <tony.face2ai@gmail.com>
2021-12-23 09:37:53 +0800

    REINFORCE algorithm for associative task

--- simple_statistical_gradient_following_algorithm_for_connectionist/associate_task.py
+++ simple_statistical_gradient_following_algorithm_for_connectionist/associate_task.py
@@ -21,31 +21,32 @@ class GaussianUnit:
     def __call__(self, input):
         self.mean = squashing_function(self.weights_mean.dot(input)) * 4
         # self.std = squashing_function(self.weights_std.dot(input))*10 + 0.01
-        action_prob = 1 / (2 * SQRT_PI * self.std) * np.exp(
-            -0.5 * ((self.action_space - self.mean) / self.std) ** 2) + 0.01
-        action_prob /= np.sum(action_prob)
+        # action_prob = 1 / (2 * SQRT_PI * self.std) * np.exp(
+        #     -0.5 * ((self.action_space - self.mean) / self.std) ** 2) + 0.01
+        # action_prob /= np.sum(action_prob)
         # print(action_prob)
-        return np.random.choice(self.action_space, 1, p=action_prob)[0]
+        # return np.random.choice(self.action_space, 1, p=action_prob)[0]
+        return np.random.normal(self.mean, self.std)
 
     def characteristic_eligibility(self, x, y):
         partial_mu = (y - self.mean) / (self.std ** 2)
         # partial_std = ((y-self.mean)**2-self.std**2)/(self.std**3)
         s = self.weights_mean.dot(x)
-        nabla_weights_mean = partial_mu * x * (np.exp(-s)/(1+np.exp(-s))**2)
+        nabla_weights_mean = partial_mu * x * (np.exp(-s) / (1 + np.exp(-s)) ** 2)
         # nabla_weights_std = partial_std * x
         return nabla_weights_mean  # , nabla_weights_std
 
 
-def reinforce_algorithm(repeat_times=100000):
+def reinforce_algorithm(repeat_times=10000000):
     # 10 arm bandits
-    K = 5
+    K = 10
     reward_log = []
     env_mean = np.random.normal(.0, 1.0, K)
 
     env = KArmedBandit(env_mean, np.ones(K))
     gu = GaussianUnit(K, range(K))
     state, reward, is_done, _ = env.step(0)
-    alpha_mean = 0.01
+    alpha_mean = 0.001
     # alpha_std = 0.01
     # base_line_std = 10
     base_line_mean = 0
@@ -53,7 +54,10 @@ def reinforce_algorithm(repeat_times=100000):
     for repeat_i in range(repeat_times):
         x = np.array(state)
         y = gu(x)
-        state, reward, is_done, _ = env.step(y)
+        if y >= K or y < 0:
+            state, reward, is_done, _ = env.step(0)
+            reward = -10.
+        state, reward, is_done, _ = env.step(int(y))
         r = reward
         # nabla_mean, nabla_std = gu.characteristic_eligibility(x, y)
         nabla_mean = gu.characteristic_eligibility(x, y)
