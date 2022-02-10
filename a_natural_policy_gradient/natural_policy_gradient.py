fda2555d25bf10d852f82c46c53508106d9471ac
Tony-Tan <tony.face2ai@gmail.com>
2022-02-10 15:37:26 +0800

    a natural policy gradient 2 states

--- a_natural_policy_gradient/natural_policy_gradient.py
+++ a_natural_policy_gradient/natural_policy_gradient.py
@@ -23,16 +23,11 @@ class NaturalPolicyGradient:
             return self.env.action_space[0]
 
     def derivative(self, x, a):
-        if x == 0:
-            forward_res = 1.0 / (1.0 + np.exp(-(self.weights[0] * a)))
-            partial_w_0 = (1 - forward_res) * forward_res * a
-            partial_w_1 = 0
-            return np.array([[partial_w_0], [partial_w_1]])
-        elif x == 1:
-            forward_res = 1.0 / (1.0 + np.exp(-(self.weights[1] * a)))
-            partial_w_0 = 0
-            partial_w_1 = (1 - forward_res) * forward_res * a
-            return np.array([[partial_w_0], [partial_w_1]])
+        forward_res_0 = 1.0 / (1.0 + np.exp(-(self.weights[0] * a)))
+        forward_res_1 = 1.0 / (1.0 + np.exp(-(self.weights[1] * a)))
+        partial_w_0 = (1 - forward_res_0) * forward_res_0 * a * (1-x)
+        partial_w_1 = (1 - forward_res_1) * forward_res_1 * a * x
+        return np.array([[partial_w_0], [partial_w_1]])
 
     def gradient_log_pi(self, x, a):
         deriv = self.derivative(x, a)
@@ -56,50 +51,53 @@ class NaturalPolicyGradient:
         return f_matrix + 1e-3 * np.eye(2)
 
     def generate_trajectory(self):
-        current_state = self.env.reset()
-        trajectory = []
-        total_reward = 0
-        for i in range(self.trajectory_horizon):
-            action = self.next_action(current_state)
-            next_state, reward, is_done, _ = self.env.step(action)
-            trajectory.append([current_state, action, reward])
-            total_reward += reward
-            current_state = next_state
-        return trajectory, total_reward
+        pass
+        #, total_reward
 
-    def q_value(self, trajectory, total_reward):
-        q_val = np.zeros((2, 2))
-        exp_eta = total_reward / self.trajectory_horizon
-        return_value = total_reward
-        current_step = 0
-        for step_i in trajectory:
-            # state = 0,1
-            # action = {-1,1}
-            # convert action to {0,1}: int((action+2)/2)
-            state, action, reward = step_i
-            if q_val[state][int((action + 2) / 2)] == 0:
-                q_val[state][int((action + 2) / 2)] = return_value / (self.trajectory_horizon - current_step) - exp_eta
-            if q_val.all() != 0:
-                break
-            return_value -= reward
-            current_step += 1
-        return q_val
+    # def q_value(self, trajectory, total_reward):
+    #     q_val = np.zeros((2, 2))
+    #     exp_eta = total_reward / self.trajectory_horizon
+    #     return_value = total_reward
+    #     current_step = 0
+    #     for step_i in trajectory:
+    #         # state = 0,1
+    #         # action = {-1,1}
+    #         # convert action to {0,1}: int((action+2)/2)
+    #         state, action, reward = step_i
+    #         if q_val[state][int((action + 2) / 2)] == 0:
+    #             q_val[state][int((action + 2) / 2)] = return_value / (self.trajectory_horizon - current_step) - exp_eta
+    #         if q_val.all() != 0:
+    #             break
+    #         return_value -= reward
+    #         current_step += 1
+    #     return q_val
 
-    def run(self, alpha):
+    def run(self, alpha, beta=0.9):
         eta_array = []
-        for i in range(1000000):
-            delta_eta = 0
-            trajectory, total_reward = self.generate_trajectory()
-            q_val = self.q_value(trajectory, total_reward)
-            for step_i in trajectory:
-                state, action, reward = step_i
-                policy_derivative = self.derivative(state, action)
-                delta_eta += policy_derivative * q_val[state][int((action + 2) / 2)]/self.sigmoid(state,action)
+        current_state = self.env.reset()
+        eligibility_trace = np.zeros((2, 1))
+        state_i_num = 0
+        total_reward = 0
+        for i in range(1, 10000000):
+            # if i % self.trajectory_horizon == 0:
             # F = policy.Fisher_matrix()
             # delta_w = np.linalg.inv(F).dot(delta_eta)
-            delta_w = delta_eta / len(trajectory)
-            self.weights += alpha * delta_w.transpose()[0]
-            if i % 10000 == 0:
+            # delta_w = delta_eta / self.trajectory_horizon
+            # self.weights += alpha * delta_w.transpose()[0]
+            # delta_eta = 0
+            # eligibility_trace = np.zeros((2, 1))
+
+            action = self.next_action(current_state)
+            next_state, reward, is_done, _ = self.env.step(action)
+            eligibility_trace = beta * eligibility_trace + \
+                                self.derivative(current_state, action) / self.sigmoid(current_state, action)
+            delta_eta = eligibility_trace * reward
+            self.weights += alpha * delta_eta.transpose()[0]
+            total_reward += reward
+            if current_state == 0:
+                state_i_num += 1
+            current_state = next_state
+            if i % 100000 == 0:
                 trans_matrix = np.zeros((2, 2))
                 trans_matrix[0][0] = self.sigmoid(0, -1)
                 trans_matrix[0][1] = self.sigmoid(0, 1)
@@ -108,25 +106,23 @@ class NaturalPolicyGradient:
                 print('---------------%dth iteration--------------------' % i)
                 print(trans_matrix)
                 print('weight:' + str(self.weights))
-                print('q value:\n' + str(q_val))
-                state_i_num = 0
-                for step_i in trajectory:
-                    state, action, reward = step_i
-                    if state == 0:
-                        state_i_num += 1
-                print('stationary distri' + str([state_i_num / self.trajectory_horizon,
-                                                 1 - state_i_num / self.trajectory_horizon]))
-                print('eta:'+str(total_reward/self.trajectory_horizon))
-                eta_array.append(total_reward/self.trajectory_horizon)
+                print('stationary distri' + str([state_i_num / i, 1 - state_i_num / i]))
+                print('eta:'+str(total_reward / 10000))
+            if i % 10000 == 0:
+                current_state = self.env.reset()
+                eligibility_trace = np.zeros((2, 1))
+                eta_array.append(total_reward / 10000)
+                total_reward = 0
         return eta_array
 
 
 if __name__ == '__main__':
     play_ground = tsMDP.TwoStatesMDP([0.8, 0.2])
     eta_matrix = []
-    for _ in range(10):
-        npg = NaturalPolicyGradient(play_ground, 20)
-        eta_array_ = npg.run(.1)
+    for epoch_i in range(1):
+        print('========================%d=========================\n\n\n'%epoch_i)
+        npg = NaturalPolicyGradient(play_ground, 50)
+        eta_array_ = npg.run(0.01)
         eta_matrix.append(eta_array_)
     eta_matrix = np.array(eta_matrix)
     plt.plot(np.average(eta_matrix, axis=0))
