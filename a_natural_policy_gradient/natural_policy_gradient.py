1468e6a9352bd1d4c9ad1152b264200d8468b772
Anthony <tony.face2ai@gmail.com>
2022-02-13 01:26:07 +0800

    2 state mdp policy gradient compare

--- a_natural_policy_gradient/natural_policy_gradient.py
+++ a_natural_policy_gradient/natural_policy_gradient.py
@@ -2,7 +2,7 @@ import matplotlib.pyplot as plt
 import numpy as np
 import random
 import two_states_MDP as tsMDP
-
+from multiprocessing import Pool
 
 class NaturalPolicyGradient:
     def __init__(self, play_ground, trajectory_horizon):
@@ -13,86 +13,55 @@ class NaturalPolicyGradient:
         # self.
 
     def sigmoid(self, x, a):
-        return 1.0 / (1.0 + np.exp(-(self.weights[0] * a))) * (1 - x) + 1.0 / (1.0 + np.exp(-(self.weights[1] * a))) * x
+        bias = 5e-3
+        return (1.-bias*2)*(1.0 / (1.0 + np.exp(-(self.weights[0] * a))) * (1 - x) + \
+               1.0 / (1.0 + np.exp(-(self.weights[1] * a))) * x) + bias
 
     def next_action(self, x):
         pro_a0 = self.sigmoid(x, -1)
-        if np.random.rand() > pro_a0:
+        if np.random.rand() >= pro_a0:
             return self.env.action_space[1]
         else:
             return self.env.action_space[0]
 
     def derivative(self, x, a):
-        forward_res_0 = 1.0 / (1.0 + np.exp(-(self.weights[0] * a)))
-        forward_res_1 = 1.0 / (1.0 + np.exp(-(self.weights[1] * a)))
-        partial_w_0 = (1 - forward_res_0) * forward_res_0 * a * (1-x)
-        partial_w_1 = (1 - forward_res_1) * forward_res_1 * a * x
+        # delta_0 = 1/(1+exp(-a*w_0))
+        # delta_1 = 1/(1+exp(-a*w_1))
+        delta_0 = self.sigmoid(0, a)
+        delta_1 = self.sigmoid(1, a)
+        partial_w_0 = (1 - delta_0) * delta_0 * a * (1 - x)
+        partial_w_1 = (1 - delta_1) * delta_1 * a * x
         return np.array([[partial_w_0], [partial_w_1]])
 
-    def gradient_log_pi(self, x, a):
-        deriv = self.derivative(x, a)
-        return deriv / self.sigmoid(x, a)
-
-    def Fisher_matrix(self,x,a):
-        gradient_log = self.derivative(x,a)/self.sigmoid(x,a)
+    def fisher_matrix(self, x, a):
+        gradient = self.derivative(x, a)
+        gradient_log = gradient / self.sigmoid(x, a)
         f_matrix = gradient_log.dot(gradient_log.transpose())
         return f_matrix
 
     def generate_trajectory(self):
         pass
-        #, total_reward
 
-    # def q_value(self, trajectory, total_reward):
-    #     q_val = np.zeros((2, 2))
-    #     exp_eta = total_reward / self.trajectory_horizon
-    #     return_value = total_reward
-    #     current_step = 0
-    #     for step_i in trajectory:
-    #         # state = 0,1
-    #         # action = {-1,1}
-    #         # convert action to {0,1}: int((action+2)/2)
-    #         state, action, reward = step_i
-    #         if q_val[state][int((action + 2) / 2)] == 0:
-    #             q_val[state][int((action + 2) / 2)] = return_value / (self.trajectory_horizon - current_step) - exp_eta
-    #         if q_val.all() != 0:
-    #             break
-    #         return_value -= reward
-    #         current_step += 1
-    #     return q_val
-
-    def run(self, alpha, beta=0.9,natural_gradient=True):
+    def run(self, alpha, random_seed, beta=0.9, natural_gradient=False):
+        np.random.seed(random_seed)
         eta_array = []
         current_state = self.env.reset()
         eligibility_trace = np.zeros((2, 1))
         state_i_num = 0
         total_reward = 0
         fisher_matrix = np.eye(2)
-        fisher_matrix_exp = np.zeros((2,2))
-        delta_eta = 0
-        horizon = 10000
+        fisher_matrix_exp = np.zeros((2, 2))
+        delta_eta = np.zeros((2, 1))
+        horizon = 100
         print_horizon = 100000
-        for i in range(1, 10000000):
-            # if i % self.trajectory_horizon == 0:
-            # F = policy.Fisher_matrix()
-            # delta_w = np.linalg.inv(F).dot(delta_eta)
-            # delta_w = delta_eta / self.trajectory_horizon
-            # self.weights += alpha * delta_w.transpose()[0]
-            # delta_eta = 0
-            # eligibility_trace = np.zeros((2, 1))
-
+        for i in range(1, 20000000):
             action = self.next_action(current_state)
             next_state, reward, is_done, _ = self.env.step(action)
+            log_gradient = self.derivative(current_state, action) / self.sigmoid(current_state, action)
             if natural_gradient:
-                fisher_matrix_exp += self.Fisher_matrix(current_state, action)
-                eligibility_trace = self.derivative(current_state, action) / self.sigmoid(current_state, action)
-                delta_eta += eligibility_trace * reward
-
-            else:
-                eligibility_trace = beta * eligibility_trace + \
-                                    self.derivative(current_state, action) / self.sigmoid(current_state, action)
-                delta_eta = eligibility_trace * reward
-                self.weights += alpha * delta_eta.transpose()[0]
-
+                fisher_matrix_exp = fisher_matrix_exp + log_gradient.dot(log_gradient.transpose())
+            eligibility_trace = beta*eligibility_trace + log_gradient
+            delta_eta += eligibility_trace * reward
             total_reward += reward
             if current_state == 0:
                 state_i_num += 1
@@ -108,29 +77,98 @@ class NaturalPolicyGradient:
                 print('weight:' + str(self.weights))
                 print('stationary distri' + str([state_i_num / i, 1 - state_i_num / i]))
                 print('eta:' + str(total_reward / horizon))
-            if i % horizon == 0:
-                current_state = self.env.reset()
-                eligibility_trace = np.zeros((2, 1))
+                print('fisher matrix')
+                print(fisher_matrix_exp / horizon)
                 eta_array.append(total_reward / horizon)
+            if i % horizon == 0:
+                # current_state = self.env.reset()
                 total_reward = 0
+                eligibility_trace = np.zeros((2, 1))
 
                 if natural_gradient:
                     delta_eta /= horizon
-                    fisher_matrix_exp = fisher_matrix_exp/horizon + 1e-3 * np.eye(2)
-                    self.weights += alpha * np.linalg.inv(fisher_matrix_exp).dot(delta_eta).transpose()[0]
+                    fisher_matrix_exp = fisher_matrix_exp / horizon + 1e-5 * np.eye(2)
+                    fisher_matrix_inv = np.linalg.inv(fisher_matrix_exp)
+                    fisher_matrix_inv /= np.max(fisher_matrix_inv)
+                    weight_delta = alpha * fisher_matrix_inv.dot(delta_eta).transpose()[0]
+                    self.weights += weight_delta
                     fisher_matrix_exp = np.zeros((2, 2))
-                    delta_eta = 0
+
+                else:
+                    delta_eta /= horizon
+                    self.weights += alpha*delta_eta.transpose()[0]
+                delta_eta = 0
         return eta_array
 
 
-if __name__ == '__main__':
+def policy_gradient(alpha, random_seed, natural_gradient_=True):
     play_ground = tsMDP.TwoStatesMDP([0.8, 0.2])
-    eta_matrix = []
-    for epoch_i in range(1):
-        print('========================%d=========================\n\n\n'%epoch_i)
-        npg = NaturalPolicyGradient(play_ground, 50)
-        eta_array_ = npg.run(.1)
-        eta_matrix.append(eta_array_)
-    eta_matrix = np.array(eta_matrix)
-    plt.plot(np.average(eta_matrix, axis=0))
+    # navilla policy gradient
+    npg = NaturalPolicyGradient(play_ground, 50)
+    eta_array_ = npg.run(.01, random_seed, natural_gradient=natural_gradient_)
+    return eta_array_
+
+
+def experiment():
+    experiment_times = 8
+    seed_seq = np.random.randint(0, 100000, experiment_times)
+    thread_num = 8
+    eta_matrix_natural = []
+    eta_matrix_navilla = []
+
+    for experiment_i in range(int(experiment_times/thread_num)):
+        pool = Pool()
+        for thread_i in range(thread_num):
+            eta_matrix_navilla.append( pool.apply_async(policy_gradient,
+                                      [0.01, seed_seq[experiment_i * thread_num+thread_i], False]))
+
+        pool.close()
+        pool.join()
+
+    for experiment_i in range(int(experiment_times/thread_num)):
+        pool = Pool()
+        for thread_i in range(thread_num):
+            eta_matrix_natural.append(pool.apply_async(policy_gradient,
+                                       [0.01, seed_seq[experiment_i * thread_num+thread_i],True]))
+        pool.close()
+        pool.join()
+    # prepare data
+    eta_matrix_natural_result = []
+    eta_matrix_navilla_result = []
+    for eta_i in eta_matrix_navilla:
+        eta_matrix_navilla_result.append(eta_i.get())
+    for eta_i in eta_matrix_natural:
+        eta_matrix_natural_result.append(eta_i.get())
+
+    eta_matrix_navilla = np.array(eta_matrix_navilla_result)
+    plt.plot(np.average(eta_matrix_navilla, axis=0), label='Navilla policy gradient, $\\alpha=0.01$')
+    eta_matrix_natural = np.array(eta_matrix_natural_result)
+    plt.plot(np.average(eta_matrix_natural, axis=0), label='Natural policy gradient, $\\alpha=0.01$')
+    plt.legend()
     plt.show()
+
+
+if __name__ == '__main__':
+    experiment()
+    # play_ground = tsMDP.TwoStatesMDP([0.8, 0.2])
+    # # navilla policy gradient
+    # eta_matrix_navilla = []
+    # for epoch_i in range(5):
+    #     print('========================%d=========================\n\n\n' % epoch_i)
+    #     npg = NaturalPolicyGradient(play_ground, 50)
+    #     eta_array_ = npg.run(.01, natural_gradient=False)
+    #     eta_matrix_navilla.append(eta_array_)
+    # eta_matrix_navilla = np.array(eta_matrix_navilla)
+    # plt.plot(np.average(eta_matrix_navilla, axis=0), label='Navilla policy gradient, $\\alpha=0.01$')
+    # # natural policy gradient
+    # eta_matrix_natural = []
+    # for epoch_i in range(5):
+    #     print('========================%d=========================\n\n\n' % epoch_i)
+    #     npg = NaturalPolicyGradient(play_ground, 50)
+    #     eta_array_ = npg.run(.01, natural_gradient=True)
+    #     eta_matrix_natural.append(eta_array_)
+    # eta_matrix_natural = np.array(eta_matrix_natural)
+    # plt.plot(np.average(eta_matrix_natural, axis=0), label='Natural policy gradient, $\\alpha=0.01$')
+    #
+    # plt.legend()
+    # plt.show()
