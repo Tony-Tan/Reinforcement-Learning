3b9cea45f8bd57f766b5d3e90efad1c2f0cba465
Anthony <tans@botinkit.com>
2024-01-23 17:48:29 +0800

    rewrite with abstractmethod

--- core/policy.py
+++ core/policy.py
@@ -3,48 +3,45 @@ from utils.commons import *
 from core.exploration import *
 from environments.envwrapper import *
 from core.replay_memory import *
+from abc import ABC, abstractmethod
 
 
-class Policy:
-    def __init__(self, env: EnvWrapper, exploration_method):
+class Policy(ABC):
+    @abstractmethod
+    def __init__(self, state_space, action_space, exploration_method):
         self.exploration = exploration_method
-        self.state_space = env.state_space
-        self.action_space = env.action_space
+        self.state_space = state_space
+        self.action_space = action_space
         pass
 
     # def select_action(self, states: np.ndarray):
     #     raise MethodNotImplement('Policy need have \'select_action\' method')
 
+    @abstractmethod
     def update(self, **kwargs):
-        raise MethodNotImplement('Policy need have \'update\' method')
+        raise NotImplementedError
 
+    @abstractmethod
     def react(self, **kwargs) -> np.ndarray:
-        raise MethodNotImplement('Policy need have \'react\' method')
+        raise NotImplementedError
 
+    @abstractmethod
     def save(self):
-        raise MethodNotImplement('Policy need have \'save\' method')
+        raise NotImplementedError
 
+    @abstractmethod
     def load(self):
-        raise MethodNotImplement('Policy need have \'load\' method')
+        raise NotImplementedError
 
 
-class ValueBasedPolicy(Policy):
+class ValueBased(ABC):
     def __init__(self, env: EnvWrapper, exploration_method):
         super().__init__(env, exploration_method)
 
     def value(self, **kwargs) -> np.ndarray:
-        raise MethodNotImplement('ValueBasedPolicy need have \'value\' method')
+        raise NotImplementedError
 
 
-class PolicyBasedPolicy(Policy):
-    def __init__(self, env: EnvWrapper, exploration_method: Exploration):
-        super().__init__(env, exploration_method)
-
-    def distribution(self, state: np.ndarray, **kwargs) -> np.ndarray:
-        raise MethodNotImplement('PolicyBasedPolicy need have \'distribution\' method')
-
-
-
-class ModelBasedPolicy(Policy):
+class ModelBased(Policy):
     def __init__(self, env: EnvWrapper, exploration_method: Exploration):
         super().__init__(env,exploration_method)
--- utils/commons.py
+++ utils/commons.py
@@ -59,14 +59,14 @@ class Callbacks:
 
 
 # exceptions
-class MethodNotImplement(Exception):
-    def __init__(self, info=None):
-        self.info = info
-
-    def __str__(self):
-        if self.info is None:
-            return 'The Method Has Not Been Implemented'
-        return self.info
+# class MethodNotImplement(Exception):
+#     def __init__(self, info=None):
+#         self.info = info
+#
+#     def __str__(self):
+#         if self.info is None:
+#             return 'The Method Has Not Been Implemented'
+#         return self.info
 
 
 class EnvNotExist(Exception):
