5517a445f8f8fd2156b0bacaeb2b81756cc649e4
Anthony Tan <tony.face2ai@gmail.com>
2024-05-11 16:23:35 +0800

    dqn comments

--- agents/dqn_agent.py
+++ agents/dqn_agent.py
@@ -1,7 +1,7 @@
 # ## DQN Agent for Atari Games
 #
 # the result of DQN agent on several atari games is shown below:
-#
+# DQN 2015
 # ![](https://raw.githubusercontent.com/Tony-Tan/Reinforcement-Learning-Data/dev/figures/DQN%202015%20rewards.png)
 # ![](https://raw.githubusercontent.com/Tony-Tan/Reinforcement-Learning-Data/dev/figures/DQN%202015%20q%20values.png)
 #
