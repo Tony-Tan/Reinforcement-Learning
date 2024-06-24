90d9ec4513c220c98d0649a80aa9af0d0d266f6c
Anthony Tan <tony.face2ai@gmail.com>
2024-06-24 17:41:26 +0800

    add atari lives counters

--- algorithms/dqn.py
+++ algorithms/dqn.py
@@ -9,7 +9,7 @@ from tools.dqn_play_ground import DQNPlayGround
 parser = argparse.ArgumentParser(description='PyTorch dqn training arguments')
 parser.add_argument('--env_name', default='ALE/Seaquest-v5', type=str,
                     help='openai gym environment (default: ALE/Atlantis-v5)')
-parser.add_argument('--device', default='cpu', type=str,
+parser.add_argument('--device', default='cuda:0', type=str,
                     help='calculation device default: cuda')
 parser.add_argument('--log_path', default='../exps/dqn/', type=str,
                     help='log save path，default: ../exps/dqn/')
